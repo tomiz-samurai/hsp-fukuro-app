4e944d54aa97eba486d64c98ce300c6adb771bf9
tomiz-samurai <tomiz.meta@gmail.com>
2025-03-10 00:44:09 +0900

    Add app entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Fukuro App Entry Point
+ * 
+ * This is the main entry point for the HSP-friendly Fukuro app.
+ * It starts the Expo app using the Expo Router.
+ */
+
+import 'expo-router/entry';
